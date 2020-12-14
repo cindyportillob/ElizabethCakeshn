b79eedf659e99180ee0b4824e6cebb075696d84a
Cindy Portillo <cindy.portillo@ujcv.edu.hn>
2020-12-14 07:02:53 -0600

    inicio

--- app/src/main/java/com/example/elizabethcakeshn/MainActivity.kt
+++ app/src/main/java/com/example/elizabethcakeshn/MainActivity.kt
@@ -19,19 +19,7 @@ class MainActivity : BaseActivity1() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_dashboard,
-                R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        navView.setupWithNavController(navController)
     }
 
 

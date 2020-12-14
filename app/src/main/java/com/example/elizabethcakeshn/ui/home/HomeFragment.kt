b25b217dfd148a68f921f0a96876f3dd1899927f
Cindy Portillo <cindy.portillo@ujcv.edu.hn>
2020-12-14 11:28:11 -0600

    DashBoard

--- app/src/main/java/com/example/elizabethcakeshn/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/elizabethcakeshn/ui/home/HomeFragment.kt
@@ -1,31 +1,49 @@
 package com.example.elizabethcakeshn.ui.home
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.elizabethcakeshn.AddProductActivity
 import com.example.elizabethcakeshn.R
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    //private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+       // homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_product_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.action_add_product) {
+            startActivity(Intent(activity, AddProductActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

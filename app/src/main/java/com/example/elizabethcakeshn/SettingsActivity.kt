7bac9589c2d1327ac8b06126e9d8bd366ebf446e
Luis Cueva <37131211+LuisCuevaBayBay@users.noreply.github.com>
2020-12-15 16:38:36 -0600

    OLIMPIA ES EL EQUIPO CON MAS CASTA DE CAMPEOOON, CONQUISTA SUS TROFEOS DENTRO Y FUERA DEL PAIS

--- app/src/main/java/com/example/elizabethcakeshn/SettingsActivity.kt
+++ app/src/main/java/com/example/elizabethcakeshn/SettingsActivity.kt
@@ -1,16 +1,26 @@
 package com.example.elizabethcakeshn
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract
+import android.view.View
+import com.example.elizabethcakes.utils.Constants
 import com.example.elizabethcakeshn.utils.GlideLoader
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.auth.User
 import kotlinx.android.synthetic.main.activity_settings.*
 
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mUserDetails: Users
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        edit_boton.setOnClickListener(this)
+        btn_cerrarSesion.setOnClickListener(this)
     }
 
     private fun getUserDetails(){
@@ -20,6 +30,10 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     fun userDetailsSuccess(users: Users){
+
+        mUserDetails = users
+
+
 
         GlideLoader(this@SettingsActivity).loadProductPicture(users.image, iv_user_photo)
         tv_nameP.text = "${users.Nombre}"
@@ -32,5 +46,26 @@ class SettingsActivity : AppCompatActivity() {
     override  fun onResume(){
         super.onResume()
         getUserDetails()
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null){
+            when(v.id){
+
+                R.id.edit_boton ->{
+                    val intent = Intent(this@SettingsActivity, UserProfileActivity::class.java)
+                    intent.putExtra(Constants.EXTRA_USER_DETAILS, mUserDetails)
+                    startActivity(intent)
+                }
+
+                R.id.btn_cerrarSesion ->{
+                    FirebaseAuth.getInstance().signOut()
+                    val intent = Intent(this@SettingsActivity, LoginActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                        finish()
+                }
+            }
+        }
     }
 }

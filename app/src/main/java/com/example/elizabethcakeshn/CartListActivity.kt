718f9747402ef69bf40198b1524c3ebf9aa5e284
AlexandroMAAC <manantial2017>
2020-12-16 15:06:54 -0600

    Diseno de pantalla carrito

---
+++ app/src/main/java/com/example/elizabethcakeshn/CartListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.elizabethcakeshn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CartListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart_list)
+    }
+
+}
